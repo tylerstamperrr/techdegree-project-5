dba6944f7f8f4fd1dfe1b8160cd64249d0af19b6
tylerstamperrr <tylergstamper@gmail.com>
2020-08-04 12:25:34 -0700

    fixed search bar

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 const search = new Filter('search', 'data-caption');
 
-window.addEventListener('load', function() {
-    baguetteBox.run('.gallery');
-  });
-
 baguetteBox.run('.gallery', {
-    animation: 'fadeIn',
+    animation: 'slideIn',
 });
